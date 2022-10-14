054e849c5c4049062d033b7f9807b372fc695d7d
tyeb11 <tayyeb.merchant@gmail.com>
2022-10-14 18:28:18 +0530

    app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
 import express from "express";
 import chalk from "chalk";
+import cookieSession from "cookie-session";
+import passport from "passport";
+import authRoutes from "./routes/auth.js";
+
+import "./util/dbconnect.js";
+import "./util/passport.js";
 
 const app = express();
+
 app.use(express.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 app.listen(process.env.PORT, () => {
   console.log(chalk.green(`server listening on port ${process.env.PORT} ...`));
