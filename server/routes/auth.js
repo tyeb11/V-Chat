2b99c7f705a275892a031960448338fa55452d03
tyeb11 <tayyeb.merchant@gmail.com>
2022-10-15 03:43:06 +0530

    auth-routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,6 +9,33 @@ export default (app) => {
       //res.redirect("/chat");
     }
   );
+  app.get(
+    "/api/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+      failureRedirect: "/",
+    })
+  );
+  app.get(
+    "/api/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+      res.redirect("/api/auth/current-user");
+    }
+  );
+
+  app.get(
+    "/api/auth/github",
+    passport.authenticate("github", { failureRedirect: "/" })
+  );
+  app.get(
+    "/api/auth/github/callback",
+    passport.authenticate("github"),
+    (req, res) => {
+      res.redirect("/api/auth/current-user");
+    }
+  );
+
   app.get("/api/auth/current-user", (req, res) => {
     res.send(req.user);
   });
