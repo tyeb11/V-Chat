738b80d40777d936ae869bc0c733363b6ef7f87b
tyeb11 <tayyeb.merchant@gmail.com>
2022-10-14 18:29:42 +0530

    modal user

---
+++ server/model/user.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validator(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      validator(value) {
+        if (value.includes("password")) {
+          throw new Error("password is too weak");
+        }
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
