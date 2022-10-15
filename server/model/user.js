070e1169ba0d413a11f9d58f6098283eb63ae1ae
tyeb11 <tayyeb.merchant@gmail.com>
2022-10-15 14:47:43 +0530

    added user model

--- server/model/user.js
+++ server/model/user.js
@@ -7,13 +7,13 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      unique: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       trim: true,
+
       validator(value) {
         if (!validator.isEmail(value)) {
           throw new Error("email is invalid");
