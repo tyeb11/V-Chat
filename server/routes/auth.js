0e1d7a5993b4b3e54fc46f099b119a6ccb30e555
tyeb11 <tayyeb.merchant@gmail.com>
2022-10-15 14:46:37 +0530

    added delete user route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 import passport from "passport";
+import { User } from "../model/user.js";
 
 export default (app) => {
   app.post(
@@ -20,7 +21,7 @@ export default (app) => {
     "/api/auth/google/callback",
     passport.authenticate("google", { failureRedirect: "/" }),
     (req, res) => {
-      res.redirect("/api/auth/current-user");
+      res.redirect("/chat");
     }
   );
 
@@ -35,8 +36,19 @@ export default (app) => {
       res.redirect("/api/auth/current-user");
     }
   );
-
+  app.delete("/api/auth/logout", async (req, res) => {
+    const user = await User.findByIdAndDelete(req.user);
+    console.log(user);
+    await req.logout();
+    req.session = null;
+    res.clearCookie("session.sig");
+    res.clearCookie("session");
+    res.redirect("/");
+  });
   app.get("/api/auth/current-user", (req, res) => {
+    res.send(req.user);
+  });
+  app.get("/chat", (req, res) => {
     res.send(req.user);
   });
 };
