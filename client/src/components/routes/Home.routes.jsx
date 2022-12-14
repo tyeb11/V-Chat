1460c92f68f91bbb3045f31bb7e42d6197d6a039
tyeb11 <tayyeb.merchant@gmail.com>
2022-12-14 15:27:17 +0530

    updated signin button component

--- client/src/components/routes/Home.routes.jsx
+++ client/src/components/routes/Home.routes.jsx
@@ -1,9 +1,13 @@
 import { Box, Typography } from "@mui/material";
+import { ReactComponent as Hero } from "../../assets/hero.svg";
 
 function Home() {
   return (
     <Box sx={{ marginTop: "100px" }}>
-      <Typography>Welcome to V-Chat</Typography>
+      <Typography variant="h5" component="h2" sx={{ marginTop: "20px" }}>
+        To Access our Services pleazz Sign in ;)
+      </Typography>
+      <Hero style={{ width: "75vw", height: "75vh" }} />
     </Box>
   );
 }
