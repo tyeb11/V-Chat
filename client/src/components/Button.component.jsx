0417adc31516c9deac103f97d8639a75be8b3d07
tyeb11 <tayyeb.merchant@gmail.com>
2022-12-15 18:15:29 +0530

    added google git cancel invertcancel button

--- client/src/components/Button.component.jsx
+++ client/src/components/Button.component.jsx
@@ -1,16 +1,34 @@
 import "../styles/Button.styles";
 
-import { SignInButton, BaseButton } from "../styles/Button.styles.js";
+import {
+  SignInButton,
+  BaseButton,
+  GoogleSignInButton,
+  GithubSignInButton,
+  CancelButton,
+  InvCancelButton,
+  SignInFormButton,
+} from "../styles/Button.styles.js";
 
 export const BUTTON_TYPE_CLASSES = {
   base: "base",
   signIn: "sign_in",
+  google_signin: "google_signin",
+  github_signin: "github_signin",
+  cancel: "cancel",
+  invcancel: "inv_cancel",
+  sign_in_form_submit: "sign_in_form_submit",
 };
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
   ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.signIn]: SignInButton,
+    [BUTTON_TYPE_CLASSES.google_signin]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.github_signin]: GithubSignInButton,
+    [BUTTON_TYPE_CLASSES.cancel]: CancelButton,
+    [BUTTON_TYPE_CLASSES.invcancel]: InvCancelButton,
+    [BUTTON_TYPE_CLASSES.sign_in_form_submit]: SignInFormButton,
   }[buttonType]);
 
 const Button = ({ children, buttonType, ...otherProps }) => {
