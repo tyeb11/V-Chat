3a8da5f76d5a489915d020ba6c0d65835beafab9
tyeb11 <tayyeb.merchant@gmail.com>
2022-12-16 19:54:52 +0530

    added chatpage component

--- client/src/components/routes/Chat.routes.jsx
+++ client/src/components/routes/Chat.routes.jsx
@@ -1,11 +1,10 @@
 import { Box, Typography } from "@mui/material";
+import ChatPage from "../ChatPage.component";
 
 function Chat() {
   return (
     <Box sx={{ marginTop: "100px" }}>
-      <Typography variant="h5" component="h2" sx={{ marginTop: "20px" }}>
-        Chat Page
-      </Typography>
+      <ChatPage />
     </Box>
   );
 }
