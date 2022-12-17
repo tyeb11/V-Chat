7f649ffc7dec35d8d26d4096d431eee38f1e06a4
tyeb11 <tayyeb.merchant@gmail.com>
2022-12-17 23:13:04 +0530

    added color theme to project

---
+++ client/src/styles/ColorTheme.styles.js
@@ -0,0 +1,21 @@
+// THEME 51 Grecian Holiday
+
+// #2988BC - Grecian Blue ---  chat and selected chat
+// #2F496E - Sea          ---  header
+// #F4EADE - Plaster      ---  chat pannel,header font color
+// #ED8C72 - Coral        ---  profileBorder,signInButton,buttons
+
+// #128277 - Deep Aqua    ---  accept buttons
+//  or
+// #506D2F - Leaves       ---  accept buttons
+//  or
+// #ACBD78 - Olive        ---  accept buttons [high priority]
+
+// #B2473E - Tuscan Red   ---  cancel buttons
+
+export const coral = "#ED8C72";
+export const grecianBlue = "#2988BC";
+export const sea = "#2F496E";
+export const plaster = "#F4EADE";
+export const olive = "#ACBD78";
+export const tuscanRed = "#B2473E";
