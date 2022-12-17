70b4cbcf6810f6955fb131cf88c6ad1164128cf4
tyeb11 <tayyeb.merchant@gmail.com>
2022-12-17 23:17:36 +0530

    added searchbar create_group_button chat_page

--- client/src/components/routes/Chat.routes.jsx
+++ client/src/components/routes/Chat.routes.jsx
@@ -1,9 +1,31 @@
 import { Box, Typography } from "@mui/material";
 import ChatPage from "../ChatPage.component";
+import SearchIcon from "@mui/icons-material/Search";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import Button from "../Button.component";
+import { BUTTON_TYPE_CLASSES } from "../Button.component";
+
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+  SearchAndGroupContainer,
+} from "../../styles/ChatPage.styles";
+import SearchBar from "../SearchBar.component";
+import { plaster } from "../../styles/ColorTheme.styles";
 
 function Chat() {
   return (
-    <Box sx={{ marginTop: "100px" }}>
+    <Box sx={{ marginTop: "56px" }}>
+      <Box sx={SearchAndGroupContainer}>
+        <SearchBar />
+        <Button
+          sx={{ top: "15px" }}
+          buttonType={BUTTON_TYPE_CLASSES.create_group}
+        >
+          <GroupAddIcon />
+        </Button>
+      </Box>
       <ChatPage />
     </Box>
   );
